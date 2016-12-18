a4cdde022a1f66759b17908d9f1c3dbcf1affc47
Gustavo Freitas de Amorim <gustavofreitasamorim@gmail.com>
2016-12-18 23:48:59 +0000

    Database definition and basic API routes defined.

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,9 @@ use Illuminate\Http\Request;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+
+Route::get('categories/{id}/topics', 'CategoryController@topics');
+Route::resource('categories', 'CategoryController');
+
+Route::resource('topics', 'TopicController');
